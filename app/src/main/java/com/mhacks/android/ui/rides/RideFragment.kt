92fa1bad939f182d050f13a5cc4caaaaadc1f0a7
Jeffrey Chang <changjef@umich.edu>
2017-09-16 23:15:17 -0400

    started on dmodel"

--- app/src/main/java/com/mhacks/android/ui/rides/RideFragment.kt
+++ app/src/main/java/com/mhacks/android/ui/rides/RideFragment.kt
@@ -1,7 +1,9 @@
 package com.mhacks.android.ui.rides
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mhacks.android.models.Ride
@@ -22,21 +24,25 @@ class RideFragment : BaseFragment() {
     override var AppBarTitle: Int = R.string.title_map
     override var LayoutResourceID: Int = R.layout.fragment_map
 
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setCustomActionBarColor(R.color.semiColorPrimary)
-        fragment_map_recycler_view.adapter = RideListAdapter()
+        fragment_map_recycler_view.adapter = RideListAdapter(
+                context,
+        )
     }
 
-    internal class RideListAdapter : RecyclerView.Adapter<RideListViewHolder> {
+    internal class RideListAdapter(val context: Context,
+                                   val rideArrayList: ArrayList<Ride>) : RecyclerView.Adapter<RideListViewHolder>() {
 
-        constructor(rideArrayList: ArrayList<Ride>) : super()
 
         override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RideListViewHolder {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            val root: View = LayoutInflater.from(context).inflate(R.layout.ride_viewholder_item, parent, false)
+
+            return RideListViewHolder(root)
         }
 
         override fun getItemCount(): Int {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            return rideArrayList.size
         }
 
         override fun onBindViewHolder(holder: RideListViewHolder?, position: Int) {
@@ -47,6 +53,10 @@ class RideFragment : BaseFragment() {
 
     internal class RideListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+    }
+
+    fun getMockData(): ArrayList<Ride> {
+        ArrayList<Ride>
     }
 
 }
