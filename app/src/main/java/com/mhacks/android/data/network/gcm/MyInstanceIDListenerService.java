eff92fd863bd3fc91bb30f75aa3db24e236507c4
Jeffrey Chang <changjef@umich.edu>
2017-09-16 23:40:14 -0400

    Commented out

--- app/src/main/java/com/mhacks/android/data/network/gcm/MyInstanceIDListenerService.java
+++ app/src/main/java/com/mhacks/android/data/network/gcm/MyInstanceIDListenerService.java
@@ -1,35 +1,35 @@
 package com.mhacks.android.data.network.gcm;
-
-import android.content.Intent;
-import android.os.IBinder;
-
-import com.google.android.gms.iid.InstanceIDListenerService;
-
-/**
- * Created by Omkar Moghe on 2/10/2016.
- */
-public class MyInstanceIDListenerService extends InstanceIDListenerService {
-
-    public MyInstanceIDListenerService() {
-        super();
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    public void onTokenRefresh() {
-        Intent intent = new Intent(this, RegistrationIntentService.class);
-        startService(intent);
-//        super.onTokenRefresh();
-    }
-
-
-}
+//
+//import android.content.Intent;
+//import android.os.IBinder;
+//
+//import com.google.android.gms.iid.InstanceIDListenerService;
+//
+///**
+// * Created by Omkar Moghe on 2/10/2016.
+// */
+//public class MyInstanceIDListenerService extends InstanceIDListenerService {
+//
+//    public MyInstanceIDListenerService() {
+//        super();
+//    }
+//
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//    }
+//
+//    @Override
+//    public void onDestroy() {
+//        super.onDestroy();
+//    }
+//
+//    @Override
+//    public void onTokenRefresh() {
+////        Intent intent = new IntentServiceå≥(this, RegistrationIntentService.class);
+//        startService(intent);
+////        super.onTokenRefresh();
+//    }
+//
+//
+//}
--- app/src/main/java/com/mhacks/android/data/network/gcm/RegistrationIntentService.java
+++ app/src/main/java/com/mhacks/android/data/network/gcm/RegistrationIntentService.java
@@ -14,29 +14,29 @@ import java.io.IOException;
 /**
  * Created by Omkar Moghe on 2/10/2016.
  */
-public class RegistrationIntentService extends IntentService {
-
-    private static final String TAG = "RegIntentService";
-
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to title the worker thread, important only for debugging.
-     */
-    public RegistrationIntentService(String name) {
-        super(name);
-    }
-
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        try {
-            InstanceID instanceID = InstanceID.getInstance(this);
-            String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
-                                               GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
-
-            Log.d(TAG, token);
-        } catch (IOException ioe) {
-            Log.e(TAG, "onHandleIntent: shit", ioe);
-        }
-    }
-}
+//public class RegistrationIntentService extends IntentService {
+//
+//    private static final String TAG = "RegIntentService";
+//
+//    /**
+//     * Creates an IntentService.  Invoked by your subclass's constructor.
+//     *
+//     * @param name Used to title the worker thread, important only for debugging.
+//     */
+//    public RegistrationIntentService(String name) {
+//        super(name);
+//    }
+//
+//    @Override
+//    protected void onHandleIntent(Intent intent) {
+//        try {
+//            InstanceID instanceID = InstanceID.getInstance(this);
+//            String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
+//                                               GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+//
+//            Log.d(TAG, token);
+//        } catch (IOException ioe) {
+//            Log.e(TAG, "onHandleIntent: shit", ioe);
+//        }
+//    }
+//}
